d8513217cc156b25abbc7db93b521b14118d44d6
Sevil Öztürk <ozsevil730@gmail.com>
2025-02-08 11:54:21 +0300

    arrange

--- models/Posts.js
+++ models/Posts.js
@@ -4,7 +4,7 @@ const postSchema = new mongoose.Schema({
   title: { type: String, required: true, unique:true },
   subtitle: String, 
   content: { type: String, required: true },
-  tags: String ,
+  tags: [String] ,
   image: String ,
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   createdAt: { type: Date, default: Date.now },
